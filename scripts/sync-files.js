e7f5abd450280b2fceaa05d6f08094e3be3ac1a3
arnoson <omasocke@hotmail.de>
2021-05-01 18:56:51 +0200

    Add file sync script

--- scripts/sync-files.js
+++ scripts/sync-files.js
@@ -5,10 +5,8 @@ import { sep, posix } from 'path'
 
 const bridge = new MiwosBridge()
 
-chokidar.watch('./lua/*.lua').on('change', async path => {
+chokidar.watch('./src/*.lua').on('change', async path => {
   const data = await fs.readFile(path)
   const posixPath = path.split(sep).join(posix.sep)
   await bridge.writeFile(posixPath, data)
-
-  bridge.sendMessage('/lua/execute-file', 'lua/index.lua')
 });
