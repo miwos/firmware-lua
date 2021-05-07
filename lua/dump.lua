432b74b728741f5c104096dd6532b8232a888d40
arnoson <omasocke@hotmail.de>
2021-05-07 02:29:31 +0200

    Send dump as json

--- lua/dump.lua
+++ lua/dump.lua
@@ -1,54 +1,78 @@
 local function quote(value) return "'" .. value .. "'" end
+local function doubleQuote(value) return '"' .. value .. '"' end
 local function bracket(value) return "[" .. value .. "]" end
 
-local function isPrimitve(var)
-  local varType = type(var)
+---Check wether a value is primitive (number, boolean, string) or complex
+---(table, function, ...)
+---@param value any
+---@return boolean
+local function isPrimitve(value)
+  local valueType = type(value)
   return (
-    varType == 'number' or
-    varType == 'string' or
-    varType == 'boolean'    
+    valueType == 'number' or
+    valueType == 'string' or
+    valueType == 'boolean'
   )
 end
 
-local function prettyValue(value)
+---Return a json-valid representation of the value.
+---@param value any
+---@return string
+local function valueToJson(value)
   local valueType = type(value)
-  local str = isPrimitve(value) and tostring(value) or valueType
-  return valueType == 'string' and quote(str) or str
+  local str = isPrimitve(value) and tostring(value) or doubleQuote(valueType)
+  return valueType == 'string' and doubleQuote(quote(str)) or str
 end
 
-local function prettyKey(key)
+---Return a json-valid representation of the key.
+---@param key any
+---@return string
+local function keyToJson(key)
   local keyType = type(key)
   local keyIsPrimitve = isPrimitve(key)
   local str = keyIsPrimitve and tostring(key) or type(key)
   return (not keyIsPrimitve or keyType == 'number') and bracket(str) or str
 end
 
-local function toPrettyString(var, depth, done)
-  depth = depth or 0
+---Convert a table into a json representation.
+---based on https://stackoverflow.com/a/64796533/12207499, thanks Francisco!
+---@param t table
+---@return string
+local function tableToJson(t, done)
   done = done or {}
-  done[var] = true
+  done[t] = true
 
-  local str = '{\n'
-  for key, value in pairs(var) do
-    str = str .. string.rep(' ', depth + 1)
-
+  local str = '{'
+  local key, value = next(t, nil)
+  while key do
     if (type(value) == 'table' and not done[value]) then
       done[value] = true
-      str = 
-        str .. prettyKey(key) .. ' = ' .. toPrettyString(value, depth + 1, done)
+      str = str .. string.format(
+        '"%s":%s',
+        keyToJson(key),
+        tableToJson(value, done)
+      )
+
       done[value] = nil
     else
-      str = str ..prettyKey(key) .. ' = ' .. prettyValue(value)
-      if done[value] then str = str .. ' (circular reference)' end
+      str = str .. 
+        string.format('"%s":%s', keyToJson(key), valueToJson(value))
     end
-    str = str .. '\n'
+
+    key, value = next(t, key)
+    if key then str = str .. ',' end
   end
-  return str .. string.rep(' ', depth) .. '}'
+  return str .. '}'
 end
 
----Dump variables for debugging.
----@param variable any
----@return string
-local function dump(variable) print(toPrettyString(variable)) end
+---Dump a value to the console.
+---@param value any
+local function dump(value)
+  -- We wrap the result of `valueToJson()` into a bracket so the dump remains
+  -- valid json.
+  Log.dump(
+    type(value) == 'table' and tableToJson(value) or bracket(valueToJson(value))
+  )
+end
 
 return dump
