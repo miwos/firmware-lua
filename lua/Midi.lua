95c513e226f6d6357b9e624b9e77bb8fbe8a4777
arnoson <omasocke@hotmail.de>
2021-06-28 20:00:25 +0200

    Use actual midi types

--- lua/Midi.lua
+++ lua/Midi.lua
@@ -1,15 +1,18 @@
+local utils = require('utils')
 -- The global Midi object might have already been created by c++.
 Midi = _G.Midi or {}
 
-Midi.TypeNoteOn = 1
-Midi.TypeNoteOff = 2
-Midi.TypeControlChange = 3
+Midi.TypeNoteOff = 0x80
+Midi.TypeNoteOn = 0x90
+Midi.TypeControlChange = 0xB0
 
 Midi.typeNames = {
   [Midi.TypeNoteOn] = 'noteOn',
   [Midi.TypeNoteOff] = 'noteOff',
   [Midi.TypeControlChange] = 'controlChange',
 }
+
+Midi.inputHandler = nil
 
 ---@alias MidiType "Midi.TypeNoteOn" | "Midi.TypeNoteOff" | "Midi.TypeControlChange"
 
@@ -42,15 +45,25 @@ function Midi.ControlChange(control, value, channel)
 end
 
 function Midi.Message(type, data)
-  return { type = type, data = data }
+  -- `Midi.typeNames` contains all supported midi message types as keys.
+  if Midi.typeNames[type] ~= nil then
+    return { type = type, data = data }
+  end
 end
 
--- Receive midi functions:
-
-function Midi.handleNoteOn(input, ...)
-  Miwos.input:output(input + 1, Midi.NoteOn(...))
+function Midi.setInputHandler(handler)
+  Midi.inputHandler = handler
 end
 
-function Midi.handleNoteOff(input, ...)
-  Miwos.input:output(input + 1, Midi.NoteOff(...))
+function Midi.removeInputHandler()
+  Midi.inputHandler = nil
+end
+
+-- Receive midi functions from c++:
+
+function Midi.handleInput(index, type, data1, data2, channel)
+  local message = Midi.Message(type, { data1, data2, channel })
+  if message then
+    utils.callIfExists(Midi.inputHandler, { index, message })
+  end
 end
