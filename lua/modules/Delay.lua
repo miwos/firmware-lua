b171b6c7a699c63ce4dca4165a5793a47ed31c7d
arnoson <omasocke@hotmail.de>
2021-06-28 19:59:59 +0200

    Format

--- lua/modules/Delay.lua
+++ lua/modules/Delay.lua
@@ -1,20 +1,20 @@
 ---@class Delay : Module
 local Delay = Miwos.createModule('Delay')
 
-Delay:defineProps{
-  time = Prop.Number{ default = 500, min = 0, max = 1000 }
-}
+Delay:defineProps({
+  time = Prop.Number({ default = 500, min = 0, max = 1000 }),
+})
 
 ---@param message MidiNoteOn
 function Delay:input1_noteOn(message)
-  Timer.schedule(Timer.now() + self.props.time, function ()
+  Timer.schedule(Timer.now() + self.props.time, function()
     self:output(1, Midi.NoteOn(unpack(message.data)))
   end)
 end
 
 ---@param message MidiNoteOff
 function Delay:input1_noteOff(message)
-  Timer.schedule(Timer.now() + self.props.time, function ()
+  Timer.schedule(Timer.now() + self.props.time, function()
     self:output(1, Midi.NoteOff(unpack(message.data)))
   end)
 end
