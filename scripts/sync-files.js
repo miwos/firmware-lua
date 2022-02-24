66b6c55b7f1fa193c5a99a7a166ae58477855b6f
arnoson <omasocke@hotmail.de>
2022-02-24 19:17:02 +0100

    Implement file syncing

--- scripts/sync-files.js
+++ scripts/sync-files.js
@@ -1,14 +1,60 @@
 import LuaOnArduino from 'lua-on-arduino'
+import { AsyncOsc } from 'async-osc'
+import NodeSerialTransport from 'async-osc/dist/NodeSerialTransport.js'
+import chokidar from 'chokidar'
+import { promises as fs } from 'fs'
 
-;(async () => {
-  const loa = new LuaOnArduino()
-  await loa.connect('COM4')
+const loa = new LuaOnArduino(new AsyncOsc(new NodeSerialTransport()))
 
-  // console.log((await loa.readFile('lua/Miwos.lua')).toString())
-  // console.log((await loa.readFile('lua/init.lua')).toString())
-  // console.log((await loa.readFile('lua/patches/patch1.lua')).toString())
+export const delay = (duration) =>
+  new Promise(resolve => setTimeout(resolve, duration))
 
-  // await loa.updateFile('lua/init.lua');
+  const pathToPosix = (path) => path.replace(/\\/g, '/')
 
-  await loa.syncFiles('lua/**/*', { watch: true, override: true, initial: false })
-})()
+const syncFiles = async (
+  pattern,
+  { watch = false, override = true, initial = false } = {}
+) => {
+  // this.logger.info(`sync files ${pattern}`)
+  const dir = await loa.readDirectory('lua')
+
+  const syncFile = async (path, update = true) => {
+    const posixPath = pathToPosix(path)
+
+    // As `updateFile()` also logs a success message we can omit the
+    // `writeFile()` success.
+    const logSuccess = !update
+
+    // For some reasons, reading the file inside a chokidar callback sometimes
+    // returns an empty string, at least on windows. Maybe the file is locked?
+    // As a (dirty) workaround we just wait a bit...
+    await delay(10)
+
+    loa.writeFile(posixPath, await fs.readFile(path), logSuccess)
+    update && loa.updateFile(posixPath)
+  }
+
+  const handleInitialAdd = (path) => {
+    const relativePath = path.substring(4) // omit the leading `lua/`
+    if (override || !dirIncludes(dir, relativePath)) syncFile(path, false)
+  }
+
+  return new Promise(resolve => {
+    const watcher = chokidar.watch(pattern)
+    initial && watcher.on('add', handleInitialAdd)
+    watcher.on('ready', async () => {
+      watcher.off('add', handleInitialAdd)
+      watcher.on('change', syncFile)
+      if (!watch) {
+        watcher.close()
+        resolve(watcher)
+      }
+    })
+  })
+}
+
+loa
+  .connect({ path: 'COM4' })
+  .then(() =>
+    syncFiles("lua/**/*", { watch: true, override: true, initial: false })
+  );
