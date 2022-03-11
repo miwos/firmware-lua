d65f562f2878c3f975cc8f57b7b477f520c58888
arnoson <omasocke@hotmail.de>
2022-03-11 18:38:37 +0100

    Add module `LowestNote`

---
+++ lua/modules/LowestNote.lua
@@ -0,0 +1,33 @@
+---@class ModuleLowestNote : Module
+local LowestNote = Modules.create('LowestNote', { shape = 'Convert' })
+
+---@param a MidiNoteOn
+---@param b MidiNoteOn
+local function getLowerNote(a, b)
+  return (b and b.note < a.note) and b or a
+end
+
+function LowestNote:init()
+  ---@type MidiNoteOn
+  self.lowestNote = nil
+  self.maxNoteInterval = 1000
+  self.lastNoteTime = 0
+  self.timerId = nil
+end
+
+LowestNote:defineInOut({ Input.Midi, Output.Midi })
+
+---@param self ModuleLowestNote
+---@param note MidiNoteOn
+LowestNote:on('input1:noteOn', function(self, note)
+  self.lowestNote = getLowerNote(note, self.lowestNote)
+
+  Timer.cancel(self.timerId)
+  self.timerId = Timer.schedule(function()
+    self:__finishNotes()
+    self:output(1, self.lowestNote)
+    self.lowestNote = nil
+  end, Timer.now() + self.maxNoteInterval)
+end)
+
+return LowestNote
